2714856c29c884218449c928d09ba366d1130270
Pierre <pi.lequay@gmail.com>
2021-08-22 11:31:31 +0200

    Classe plat + categorie

---
+++ src/main/java/model/Categorie.java
@@ -0,0 +1,5 @@
+package model;
+
+public enum Categorie {
+Apero, entree, plaat, dessert, autres
+}
--- src/main/java/model/Plat.java
+++ src/main/java/model/Plat.java
@@ -1,5 +1,69 @@
 package model;
 
-public class Plat {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Plat {
+	
+	private int id;
+	private String nom;
+	private List<Recette> recettes = new ArrayList();
+	private Categorie categorie;
+	
+	public Plat() {
+		super();
+	}
+
+	public Plat(String nom, List<Recette> recettes, Categorie categorie) {
+		super();
+		this.nom = nom;
+		this.recettes = recettes;
+		this.categorie = categorie;
+	}
+
+	public Plat(int id, String nom, List<Recette> recettes, Categorie categorie) {
+		super();
+		this.id = id;
+		this.nom = nom;
+		this.recettes = recettes;
+		this.categorie = categorie;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public List<Recette> getRecettes() {
+		return recettes;
+	}
+
+	public void setRecettes(List<Recette> recettes) {
+		this.recettes = recettes;
+	}
+
+	public Categorie getCategorie() {
+		return categorie;
+	}
+
+	public void setCategorie(Categorie categorie) {
+		this.categorie = categorie;
+	}
+
+	@Override
+	public String toString() {
+		return "Plat [id=" + id + ", nom=" + nom + ", recettes=" + recettes + ", categorie=" + categorie + "]";
+	}
+	
 }
